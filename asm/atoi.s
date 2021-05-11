5241a4923896c93b0d31bc6c14a85a9adea1eab2
shoyaaa <codevagus@gmail.com>
2021-05-11 22:14:54 +0300

    chore: add comments to atoi function

--- asm/atoi.s
+++ asm/atoi.s
@@ -3,19 +3,23 @@
 .text
   # int _atoi(char*)
   _atoi:
-    xor %rax, %rax
+    xor %rax, %rax # clear return value
 
     .loop_start:
       .loop:
-        movzbq (%rdi), %rsi
-        cmp $0, %rsi
+        movzbq (%rdi), %rsi # move current char to rsi
+        cmp $0, %rsi # check end of string
         je .loop_end
 
+        # ascii -> decimal
         sub $'0', %rsi
         imul $10, %rax
         add %rsi, %rax
 
+        # move next character
         inc %rdi
+
+        # loop back
         jmp .loop
 
     .loop_end:
