db0ab8c045f9a9ff93f6ca6d373573a36b76ab7b
shoyaaa <codevagus@gmail.com>
2021-05-13 18:21:21 +0300

    feat: improve build script

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Assembly x64 implementation of the C standard library using NASM.
 # Build
 \*.asm files in this repository cannot work alone. Therefore, there is a sample `main.c` file in the repository.
 ```sh
-sh ./build.sh
-./test
+./libc.sh brun
 ```
 
---
+++ libc.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+function build {
+  FILES=asm/*.asm
+  for file in $FILES; do
+    echo "Assembling $(basename -s .asm $file)"
+    nasm -f elf64 $file -o build/$(basename -s .asm $file).o
+  done
+
+  echo "Compiling main.c"
+  gcc -c main.c -o build/main.o -nostdlib
+  echo "Linking all object files."
+  gcc build/*.o -o test -nostdlib
+
+  echo "Builded"
+}
+
+function clean {
+  rm -f build/*
+  echo "Cleaned"
+}
+
+function brun {
+  build
+  echo "Starting..."; echo
+  ./test
+}
+
+if [ "$1" == "build" ] || [ -z "$1" ]; then
+  build
+elif [ "$1" == "brun" ]; then
+  brun
+elif [ "$1" == "clean" ]; then
+  clean
+fi
+
